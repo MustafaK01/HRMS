0cc12af38f25bfb4697744a75860f396c23c61ff
Mustafa Köroğlu <62333156+MustafaK01@users.noreply.github.com>
2021-12-23 08:13:38 +0300

    Changed @Component to @Service

--- src/main/java/kodlamaio/hrms/business/concretes/JobManager.java
+++ src/main/java/kodlamaio/hrms/business/concretes/JobManager.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.IJobService;
 import kodlamaio.hrms.dataAccess.abstracts.IJobDao;
 import kodlamaio.hrms.entities.concretes.Jobs;
 
-@Component
+@Service
 public class JobManager implements IJobService{
 
 	private IJobDao jobDao;
