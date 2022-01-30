cbba71fff366eb3595cac7c97c10c4d18ada59a4
Mustafa Köroğlu <62333156+MustafaK01@users.noreply.github.com>
2022-01-30 23:50:01 +0300

    Updated

--- src/main/java/kodlamaio/hrms/business/abstracts/IUserService.java
+++ src/main/java/kodlamaio/hrms/business/abstracts/IUserService.java
@@ -7,6 +7,6 @@ import kodlamaio.hrms.core.utils.results.ResultData;
 import kodlamaio.hrms.entities.concretes.User;
 
 public interface IUserService {
-	ResultData<List<User>> getAll();//bir get fonksiyonu olduğu için ve data getirip 'sonuç' göndereceği için ResultData tipinde tanımlandı. 
-	Result add(User user);//Result döndürecek aynı zamanda veri set edecek. Veri set ettiği, get etmediği için Result tipinde tanımlandı.
+	ResultData<List<User>> getAll();
+	Result add(User user);
 }
